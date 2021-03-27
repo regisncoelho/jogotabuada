10081e59effa262c881a57bf0d19c747f13ab3b1
regisncoelho <81445653+regisncoelho@users.noreply.github.com>
2021-03-27 02:33:58 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,26 @@
+var resultado = 0
+var multiplo1 = 0
+var multiplo2 = 0
+
+function geraMultiplos() {
+  multiplo1 = parseInt(Math.random() * 10)
+  multiplo2 = parseInt(Math.random() * 10)
+  resultado = multiplo1*multiplo2
+  mostraMultiplos.innerHTML = (multiplo1 + " x " + multiplo2 + " =")
+}
+  function conferir() {
+  mostraMultiplos.innerHTML = (multiplo1 + " x " + multiplo2 + " = " + resultado)
+  var camporesposta = document.querySelector("#resposta")
+  var respostainput = camporesposta.value
+  if (respostainput==resultado){
+  mostraResposta.innerHTML = ("Você respondeu: " + respostainput + "<br>" + "Parabéns! Resposta correta!") 
+  } else {
+    mostraResposta.innerHTML = ("Você respondeu: " + respostainput + "<br>" + "Que pena, você errou. ")
+  }
+    camporesposta.value = ""
+  }
+
+function rerun() {
+  mostraResposta.innerHTML = ""
+  mostraMultiplos.innerHTML = ""
+}
